b7d90a9544e18c78ed03d218668d7ed64cfe6b05
rentzepis <rentz3141@gmail.com>
2023-10-05 11:16:31 -0400

    fixing alignment

--- board.py
+++ board.py
@@ -2,8 +2,8 @@ import tkinter as tk
 
 # initialize instance variables
 stones = [ 4, 4, 4, 4, 4, 4, 0, 4, 4, 4, 4, 4, 4, 0]
-holes = [None for _ in range(14)]
 #         player 1             player 2
+holes = [None for _ in range(14)]
 
 root = tk.Tk()
 root.title("Mancala")
@@ -14,8 +14,8 @@ for i in range(2):
         normalhole = tk.Button(root, text=stones[i*6 + j], height= 3, width=6)
         normalhole.grid(row=j+2, column=i, padx=5, pady=5)
         holes[ i*6 + j ] = normalhole
-    mancala = tk.Button(root, text=stones[i*6 + j], height= 3, width=12)
-    mancala.grid(row=2 + 6*(i), column=0, padx=5, pady=5)
-    holes[6 + i*6]
+    mancala = tk.Button(root, text=stones[6+ i*6], height= 3, width=20)
+    mancala.grid(row=1 + 7*(i), column=0, padx=5, pady=5)
+    holes[6 + i*6] = mancala
 
 root.mainloop()
