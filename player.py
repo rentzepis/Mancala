4cbdfde09ba0e32e395e1da9f8e45339adc41e1b
rentzepis <rentz3141@gmail.com>
2023-10-10 09:01:12 -0400

    began a method to edit # of stones, turned player + board into classes

--- player.py
+++ player.py
@@ -1,25 +1,25 @@
-import random as random
-import tkinter as tk
-from tkinter import simpledialog
+class Player:
+    import random as random
+    import tkinter as tk
+    from tkinter import simpledialog
 
+    Root = tk.Tk()
+    Root.withdraw()
 
-Root = tk.Tk()
-Root.withdraw()
+    player1Name = simpledialog.askstring(title="Welcome", prompt='First player, type your name: ')
 
-player1Name = simpledialog.askstring(title="Welcome", prompt='First player type your name: ')
+    print("Hello,", player1Name)
 
-print("Hello", player1Name)
+    player2Name = simpledialog.askstring(title="Welcome", prompt='Second player, type your name: ')
 
-player2Name = simpledialog.askstring(title="Welcome", prompt='Second player type your name: ')
+    print("Hi,", player2Name)
 
-print("Hi", player2Name)
+    print("Flipping a coin to decide who goes first...")
+    print("Ready...")
+    print("3")
+    print("2")
+    print("1")
 
-print("Now we will flip a coin to decide who goes first")
-print("Ready...")
-print("3")
-print("2")
-print("1")
-
-coinFlip = random.randint(0, 1)
-if coinFlip == 0 :
-    P
+    coinFlip = random.randint(0, 1)
+    if coinFlip == 0:
+        x=3
