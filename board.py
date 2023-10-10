f611720c6e819e5746f454ef6445e2942dde0644
rentzepis <rentz3141@gmail.com>
2023-10-10 08:29:20 -0400

    board --> horizontal

--- board.py
+++ board.py
@@ -12,10 +12,10 @@ root.title("Mancala")
 for i in range(2):
     for j in range(6):
         normalhole = tk.Button(root, text=stones[i*6 + j], height= 3, width=6)
-        normalhole.grid(row=j+2, column=i, padx=5, pady=5)
+        normalhole.grid(row=i, column=j+1, padx=5, pady=5)
         holes[ i*6 + j ] = normalhole
     mancala = tk.Button(root, text=stones[6+ i*6], height= 3, width=20)
-    mancala.grid(row=1 + 7*(i), column=0, padx=5, pady=5)
+    mancala.grid(row=0, column=7*i, padx=5, pady=5)
     holes[6 + i*6] = mancala
 
 root.mainloop()
