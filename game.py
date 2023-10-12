a5ebc69f2b8719a2d3dc02fbdd20df5056a057be
Natalia <vilelamnatalia@gmail.com>
2023-10-12 12:14:44 -0400

    ideas in game.py

--- game.py
+++ game.py
@@ -13,3 +13,15 @@ def holeClick(self, i):
                 tk.messagebox.showerror(title='error', message='pick a hole in your own side')
 
                 break
+
+    if player2turn == True:
+        for x in range (7,14):
+            if i == x:
+                pickedup = int(self.buttons[i].cget("text"))
+                self.buttons[i].config(text=0)
+                for x in range(pickedup):
+                    i = i + 1
+                    self.incrementStoneCount(i)
+            else:
+                tk.messagebox.showerror(title='error', message='pick a hole in your own side')
+                break
